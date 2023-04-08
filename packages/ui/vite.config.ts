dd99bb658372c4a4566d7af7fa653ecee6315b80
Riderman de Sousa Barbosa <725337+Ridermansb@users.noreply.github.com>
2023-04-08 09:57:52 -0300

    include posthtml
    Signed-off-by: Riderman de Sousa Barbosa <725337+Ridermansb@users.noreply.github.com>

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,10 +2,46 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { checker } from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
+import posthtml from "posthtml";
+
+const pluginAddAppCheckDebugToken: posthtml.Plugin<unknown> = function (tree) {
+  tree.match({ attrs: { id: "appcheck" } }, function (node) {
+    node.content = ["self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;"];
+    return node;
+  });
+
+  return tree;
+};
+
+function vitePluginPosthtml(plugins: posthtml.Plugin<never>[]) {
+  return {
+    name: "vite-plugin-posthtml",
+    async transformIndexHtml(html: string) {
+      const { html: transformedHtml } = await posthtml(plugins).process(html);
+      return transformedHtml;
+    },
+  };
+}
+
+function loadPosthtmlPlugins(mode: string, command: string) {
+  const isPreview = mode === "staging" && command === "build";
+  const isDevelopment = mode === "development";
+  if (isDevelopment || isPreview) {
+    console.warn("Using development/preview configuration");
+    return [pluginAddAppCheckDebugToken];
+  }
+
+  return [];
+}
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode, command }) => {
   const plugins = [tsconfigPaths(), react()];
+
+  const posthtmlPlugins = loadPosthtmlPlugins(mode, command);
+  if (posthtmlPlugins.length > 0) {
+    plugins.push(vitePluginPosthtml(posthtmlPlugins));
+  }
 
   const isTest = process.env.NODE_ENV === "test" || process.env.VITEST;
   if (!isTest) {
