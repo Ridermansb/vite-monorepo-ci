7756961f930db00c21ca46d0f1aec928af8b37e8
Riderman de Sousa Barbosa <725337+Ridermansb@users.noreply.github.com>
2023-04-08 09:52:16 -0300

    add eslint
    Signed-off-by: Riderman de Sousa Barbosa <725337+Ridermansb@users.noreply.github.com>

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { checker } from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  const plugins = [tsconfigPaths(), react()];
+
+  const isTest = process.env.NODE_ENV === "test" || process.env.VITEST;
+  if (!isTest) {
+    plugins.unshift(
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: "eslint --ext .ts,.tsx,.js,.jsx .",
+        },
+      })
+    );
+  }
+
+  return {
+    plugins,
+  };
+});
